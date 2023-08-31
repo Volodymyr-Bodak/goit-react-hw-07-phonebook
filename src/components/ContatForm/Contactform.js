fd6d3a96c336213920ba4ba6f0d3625ae19338ca
Volodymyr-Bodak <122619914+Volodymyr-Bodak@users.noreply.github.com>
2023-08-31 13:44:52 +0200

    xd

--- src/components/ContatForm/Contactform.js
+++ src/components/ContatForm/Contactform.js
@@ -1,45 +1,53 @@
 import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { addContact } from 'redux/phonebookSlice';
+import styles from './ContactForm.module.css';
 
 const ContactForm = () => {
-  const dispatch = useDispatch();
   const [name, setName] = useState('');
   const [phone, setPhone] = useState('');
+  const contacts = useSelector(state => state.phonebook.contacts.items);
+
+  const dispatch = useDispatch();
 
   const handleSubmit = event => {
     event.preventDefault();
 
-    if (!name || !phone) {
-      alert('Please fill in all fields.');
+    if (name.trim() === '' || phone.trim() === '') {
       return;
     }
 
-    const newContact = { name, phone };
-    dispatch(addContact(newContact));
+    const isContactExists = contacts.some(contact => contact.name.toLowerCase() === name.toLowerCase());
+
+    if (isContactExists) {
+      alert(`${name} is already in contacts`);
+      return;
+    }
+
+    dispatch(addContact({ name, phone }));
     setName('');
     setPhone('');
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <input
-          type="text"
-          placeholder="Name"
-          value={name}
-          onChange={e => setName(e.target.value)}
-        />
-      </div>
-      <div>
-        <input
-          type="text"
-          placeholder="Phone"
-          value={phone}
-          onChange={e => setPhone(e.target.value)}
-        />
-      </div>
-      <button type="submit">Add Contact</button>
+    <form className={styles.formContainer} onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className={styles.formInput}
+        placeholder="Name"
+        value={name}
+        onChange={e => setName(e.target.value)}
+      />
+      <input
+        type="text"
+        className={styles.formInput}
+        placeholder="Phone"
+        value={phone}
+        onChange={e => setPhone(e.target.value)}
+      />
+      <button type="submit" className={styles.formButton}>
+        Add Contact
+      </button>
     </form>
   );
 };
